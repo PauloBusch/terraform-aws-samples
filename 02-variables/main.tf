b250d8a72d561697f554b31a4ed9b4bf761ffb3e
PauloBusch <paulo202015@outlook.com.br>
2024-05-23 00:24:55 -0300

    feat: add 02-variables

---
+++ 02-variables/main.tf
@@ -0,0 +1,16 @@
+variable "environment" {
+    default = "dev"
+}
+
+variable "iam_user_name_prefix" {
+    default = "my_iam_user"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_iam_user" "my_iam_user" {
+  count = 3
+  name  = "${var.iam_user_name_prefix}_${count.index}"
+}

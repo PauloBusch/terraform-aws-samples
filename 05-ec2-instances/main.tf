f11b88ec7076ae039722eb4c61d43081a1276d32
PauloBusch <paulo202015@outlook.com.br>
2024-05-23 22:03:19 -0300

    feat: configured ec2 instance

--- 05-ec2-instances/main.tf
+++ 05-ec2-instances/main.tf
@@ -1,34 +1,61 @@
 provider "aws" {
-    region = "us-east-1"
-    version = "~> 2.46"
+  region  = "us-east-1"
+  version = "~> 2.46"
+}
+
+resource "aws_default_vpc" "default" {
+
 }
 
 resource "aws_security_group" "http_server_sg" {
+  name   = "http_server_sg"
+  vpc_id = aws_default_vpc.default.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
     name = "http_server_sg"
-    vpc_id = "vpc-0ca65fbc837be742b"
+  }
+}
 
-    ingress {
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+resource "aws_instance" "http_server" {
+  ami                    = "ami-04ff98ccbfa41c9ad"
+  key_name               = "default-ec2"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.http_server_sg.id]
+  subnet_id              = data.aws_subnets.default_subnets.ids[0]
 
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ec2-user"
+    private_key = file(var.aws_key_pair)
+  }
 
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = -1
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    tags = {
-        name = "http_server_sg"
-    }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum install httpd -y",
+      "sudo service httpd start",
+      "echo Virtual Server is at ${self.public_dns} | sudo tee /var/www/html/index.html"
+    ]
+  }
 }

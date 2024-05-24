e50540410b6fdc9b2a49cb0db1273dcd89a3014e
PauloBusch <paulo202015@outlook.com.br>
2024-05-24 01:13:00 -0300

    feat: add 07-backend-state

---
+++ 07-backend-state/project/main.tf
@@ -0,0 +1,17 @@
+terraform {
+    backend "s3" {
+        bucket = "application-name-backend-state"
+        key = "dev/07-backend-state/project"
+        region = "us-east-1"
+        dynamodb_table = "dev_application_locks"
+        encrypt = true
+    }
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_iam_user" "my_iam_user" {
+    name = "my_iam_user_abc"
+}
